790b7f7982bf5f243705628757a29d042b19bb59
Jerome Matthews <jerome_matthews@live.com>
2023-06-01 14:59:33 +0900

    site update: added js functionality.

---
+++ main.js
@@ -0,0 +1,16 @@
+
+//NOTE: DOM Element selection: 
+
+let mobileNav = document.querySelector(".sitenav__mobile-nav");
+let heroButton = document.querySelector(".hero-button");
+
+
+
+
+//NOTE: Interactivity 
+mobileNav.addEventListener("click", () => {
+ alert("JS is working!");
+});
+
+
+
