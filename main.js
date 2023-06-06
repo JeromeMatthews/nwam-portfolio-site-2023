a1179d14e9e65c4bd46a805ed47ea6818c1bc236
Jerome Matthews <jerome_matthews@live.com>
2023-06-06 13:28:01 +0900

    site update: fixed mobile nav overlay, file names.

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 //NOTE: DOM Element selection:
 
-let mobileNav = document.querySelector(".sitenav__mobile-nav");
+let mobileNavHamburger = document.querySelector(".sitenav__hamburger");
 let mobileOverlay = document.querySelector(".sitenav__overlay");
+let siteLinks = document.querySelector(".sitenav__links");
 let heroButton = document.querySelector(".hero-button");
 
 //NOTE: Interactivity
-mobileNav.addEventListener("click", () => {
+mobileNavHamburger.addEventListener("click", () => {
 	mobileOverlay.classList.add("--mobile-overlay");
+    siteLinks.classList.remove("sitenav__links--hide");
+    siteLinks.classList.add(".sitenav__links--show");
 });
 
 //NOTE: Turns off the overlay once any area outside of menu items are clicked. 
 mobileOverlay.addEventListener("click", () => {
-	mobileOverlay.classList.toggle("--mobile-overlay");
+	mobileOverlay.classList.remove("--mobile-overlay");
+    siteLinks.classList.remove(".sitenav__links--show")
+    siteLinks.classList.add("sitenav__links--hide")
 });
