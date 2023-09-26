0b490079da70f1581d259ed482f9f0294539ace0
Jerome Matthews <jerome_matthews@live.com>
2023-09-26 21:35:20 +0900

    site update: adjustments to mobile and tablet nav.

--- main.js
+++ main.js
@@ -5,16 +5,28 @@ let mobileOverlay = document.querySelector(".sitenav__overlay");
 let siteLinks = document.querySelector(".sitenav__links");
 let heroButton = document.querySelector(".hero-button");
 
+
+
+// let navWidth = siteLinks.offsetWidth; //full width of the navigation menu inclusive of any additions in margin or padding.
+// let vWidth = window.innerWidth; // value of the actual viewable area in viewport within the browser window.
+// let percentageOffset = 100 / navWidth; // Percentage of offset to assign, in decimal.
+// let navPosition = vWidth * percentageOffset;
+
+
+
 //NOTE: Interactivity
 mobileNavHamburger.addEventListener("click", () => {
 	mobileOverlay.classList.add("--mobile-overlay");
-    siteLinks.classList.remove("sitenav__links--hide");
-    siteLinks.classList.add(".sitenav__links--show");
+	siteLinks.classList.remove("sitenav__links--hide");
+	siteLinks.classList.add(".sitenav__links--show");
+    // siteLinks.style.setProperty("left:" + navPosition + "px");
 });
 
-//NOTE: Turns off the overlay once any area outside of menu items are clicked. 
+//NOTE: Turns off the overlay once any area outside of menu items are clicked.
 mobileOverlay.addEventListener("click", () => {
 	mobileOverlay.classList.remove("--mobile-overlay");
-    siteLinks.classList.remove(".sitenav__links--show")
-    siteLinks.classList.add("sitenav__links--hide")
+	siteLinks.classList.remove(".sitenav__links--show");
+	siteLinks.classList.add("sitenav__links--hide");
 });
+
+
