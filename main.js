dc34d3736cf9caa3a317c3df6e8748f2d5517ae5
Jerome Matthews <jerome_matthews@live.com>
2023-11-19 22:02:34 +0900

    work on base page turn ui/ux stuff - hyperlinking

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+//NOTE: We need to wait for the page to fully load and give access to all the DOM elements to be able to manipulate it, POST being sent from the server.
+window.onload = () => {
 //NOTE: DOM Element selection:
-
 let mobileNavHamburger = document.querySelector(".sitenav__hamburger");
 let mobileOverlay = document.querySelector(".sitenav__overlay");
 let siteLinks = document.querySelector(".sitenav__links");
 let heroButton = document.querySelector(".hero-button");
 
+//NOTE: DOM Elements for UI/UX Page turn animations:
+let page_container = document.querySelector(".page-container");
+let navAnchors = document.querySelectorAll("a");
 
+
+
+//NOTE: Explanation for the code to help determine the actual size of the viewport available to design with. 
 
 // let navWidth = siteLinks.offsetWidth; //full width of the navigation menu inclusive of any additions in margin or padding.
 // let vWidth = window.innerWidth; // value of the actual viewable area in viewport within the browser window.
@@ -31,3 +38,29 @@ mobileOverlay.addEventListener("click", () => {
 
 
 //ANIMATION AND UI/ UX CHANGES:
+//NOTE: Page turn animation - on window load.
+
+for (let i = 0; i < navAnchors.length; i++){
+
+	let anchor = navAnchors[i];
+
+	anchor.addEventListener("click", e => {
+		 e.preventDefault();
+		
+
+		let targetUrl = e.target.href;
+		
+		page_container.classList.add("page-flipped");
+
+
+		setTimeout(() =>{
+			
+			window.location.href = targetUrl;
+
+		}, 500);
+
+	});
+
+
+};
+};//end window onload
